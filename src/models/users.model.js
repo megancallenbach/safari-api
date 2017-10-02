3024e2afbcd08ec5e49cef74686114d34b5cccbd
Wouter de Vos <wouter@codaisseur.com>
2017-10-02 11:38:26 +0200

    Add name to users

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -5,11 +5,9 @@
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const users = new mongooseClient.Schema({
-  
-    email: {type: String, unique: true},
+    name: { type: String, required: true },
+    email: { type: String, unique: true },
     password: { type: String },
-  
-  
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
